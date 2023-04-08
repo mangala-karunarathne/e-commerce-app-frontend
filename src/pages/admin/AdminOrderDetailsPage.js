82257306dc5b2c36f25546fdca0cd4538315f25c
mangala-karunarathne <mangalaicc@gmail.com>
2023-04-08 16:14:51 +0530

    CHG | Create Protected Routes for Admins

---
+++ src/pages/admin/AdminOrderDetailsPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AdminOrderDetailsPage = () => {
+  return (
+    <div>
+      <p> This is the Admin Order Details Page</p>
+    </div>
+  )
+}
+
+export default AdminOrderDetailsPage
---
+++ src/pages/admin/AdminOrdersPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AdminOrdersPage = () => {
+  return (
+    <div>
+     <p> This is the Admin Orders Page</p> 
+    </div>
+  )
+}
+
+export default AdminOrdersPage
