d1fe007b5da3ff3750c7b0e60033b450f438b092
mangala-karunarathne <mangalaicc@gmail.com>
2023-04-08 14:08:55 +0530

    CHG |Create  Protected Routes for Regular Users

---
+++ src/pages/user/UserProfilePage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UserProfilePage = () => {
+  return (
+    <div>
+      <p> This is the User Profile Page</p>
+    </div>
+  )
+}
+
+export default UserProfilePage
