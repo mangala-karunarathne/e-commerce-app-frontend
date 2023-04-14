a4f9df7cbe1a3ff4fc90c5aa68b3b60829f12f3a
mangala-karunarathne <mangalaicc@gmail.com>
2023-04-14 14:19:40 +0530

    CHG | Product List Page Customizations Part I

--- src/pages/ProductListPage.js
+++ src/pages/ProductListPage.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import { Button, Col, Container, ListGroup, Pagination, Row } from "react-bootstrap";
 
 const ProductListPage = () => {
   return (
-    <div><p> This is the Product List Page</p></div>
-  )
-}
+    <Container fluid>
+      <Row>
+        <Col md={3}>
+          <ListGroup>
+            <ListGroup.Item>Cras justo odio</ListGroup.Item>
+            <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+            <ListGroup.Item>Morbi leo risus</ListGroup.Item>
+            <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+            <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+            <ListGroup.Item>
+            <Button variant="primary">Primary</Button>
+              <Button variant="danger">Danger</Button>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={9}>
+          {/* <ProductForListComponent />
+          <PaginationComponent /> */}
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
-export default ProductListPage
+export default ProductListPage;
