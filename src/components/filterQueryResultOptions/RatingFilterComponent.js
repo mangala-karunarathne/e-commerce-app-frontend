d7e7e81d091551ea20ca2478d65f51b561cb0eea
mangala-karunarathne <mangalaicc@gmail.com>
2023-04-16 19:29:45 +0530

    CHG | Star Rating System

--- src/components/filterQueryResultOptions/RatingFilterComponent.js
+++ src/components/filterQueryResultOptions/RatingFilterComponent.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { Form } from "react-bootstrap";
+import { Rating } from "react-simple-star-rating";
 
 const RatingFilterComponent = () => {
   return (
-    <div>
-      <p>Rating Filter Component</p>
-    </div>
+    <>
+      <span className="fw-bold">Rating</span>
+      {Array.from({ length: 5 }).map((_, idx) => (
+      <Form.Check type="checkbox" id={`check-api-${idx}`}>
+        <Form.Check.Input type="checkbox" isValid />
+        <Form.Check.Label style={{ cursor: "pointer" }}></Form.Check.Label>
+        <Rating readonly size={20} initialValue={5-idx} />
+      </Form.Check>
+      ))}
+    </>
   );
 };
 
