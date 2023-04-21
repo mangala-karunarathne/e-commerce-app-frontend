e5129534c7b7fffe9173e9c3e0fa2ab163e423a8
mangala-karunarathne <mangalaicc@gmail.com>
2023-04-21 23:35:02 +0530

    Sorting and Filtering Products I

--- src/components/filterQueryResultOptions/PriceFilterComponent.js
+++ src/components/filterQueryResultOptions/PriceFilterComponent.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Form } from "react-bootstrap";
+import FormRange from "react-bootstrap/esm/FormRange";
 
 const PriceFilterComponent = () => {
   return (
     <>
-      <Form.Label>Range</Form.Label>
-      <Form.Range />
+      <Form.Label>
+        <span className="fw-bold">Price no greater than:</span> 500$
+      </Form.Label>
+      <FormRange min={10} max={1000} step={10}/>
     </>
   );
 };
