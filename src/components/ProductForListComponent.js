1651b4a2c55347d891d3a7c6a32662b0659aedd2
mangala-karunarathne <mangalaicc@gmail.com>
2023-04-22 12:38:29 +0530

    CHG | Improve Product List and Pagination

--- src/components/ProductForListComponent.js
+++ src/components/ProductForListComponent.js
@@ -1,20 +1,38 @@
-import React from 'react'
-import { Button, Card } from 'react-bootstrap'
+import React from "react";
+import { Button, Card, Col, Row } from "react-bootstrap";
+import { Rating } from "react-simple-star-rating";
+import { LinkContainer } from "react-router-bootstrap";
 
-const ProductForListComponent = () => {
+const ProductForListComponent = ({images,idx}) => {
   return (
-    <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src="/images/books.jpg" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
+    <Card style={{ marginTop: "30px", marginBottom: "50px" }}>
+      <Row>
+        <Col lg={5}>
+          <Card.Img variant="top" src={"/images/"+ images[idx] + ".jpg"} />
+        </Col>
+        <Col lg={7}>
+          <Card.Body>
+            <Card.Title>Lorem ipsum dolor sit amet.</Card.Title>
+            <Card.Text>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos vel
+              commodi optio dignissimos laudantium temporibus, laboriosam, earum
+              vero dolore, pariatur porro possimus quaerat! Illo, eaque?
+            </Card.Text>
+            <Card.Text>
+              <Rating readonly size={20} initialValue={5} />
+              (1)
+            </Card.Text>
+            <Card.Text className="h4">
+              $125{" "}
+              <LinkContainer to="/product-details">
+                <Button variant="danger">See Product</Button>
+              </LinkContainer>
+            </Card.Text>
+          </Card.Body>
+        </Col>
+      </Row>
     </Card>
-  )
-}
+  );
+};
 
-export default ProductForListComponent
+export default ProductForListComponent;
