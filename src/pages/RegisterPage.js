c66c2514efd8457df5a9837f21e94474f7554ff5
mangala-karunarathne <mangalaicc@gmail.com>
2023-05-01 07:24:43 +0530

    CHG | Adding Frontend Validations

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
-import { Button, Col, Container, Form, InputGroup, Row } from "react-bootstrap";
-
+import { Container, Row, Col, Form, Button, Alert } from "react-bootstrap";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import Spinner from 'react-bootstrap/Spinner'
 const RegisterPage = () => {
   const [validated, setValidated] = useState(false);
 
@@ -19,75 +20,95 @@ const RegisterPage = () => {
         <Col md={6}>
           <h1>Register</h1>
           <Form noValidate validated={validated} onSubmit={handleSubmit}>
-            <Row className="mb-3">
-              <Form.Group as={Col} md="4" controlId="validationCustom01">
-                <Form.Label>First name</Form.Label>
-                <Form.Control
-                  required
-                  type="text"
-                  placeholder="First name"
-                  defaultValue="Mark"
-                />
-                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group as={Col} md="4" controlId="validationCustom02">
-                <Form.Label>Last name</Form.Label>
-                <Form.Control
-                  required
-                  type="text"
-                  placeholder="Last name"
-                  defaultValue="Otto"
-                />
-                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group as={Col} md="4" controlId="validationCustomUsername">
-                <Form.Label>Username</Form.Label>
-                <InputGroup hasValidation>
-                  <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
-                  <Form.Control
-                    type="text"
-                    placeholder="Username"
-                    aria-describedby="inputGroupPrepend"
-                    required
-                  />
-                  <Form.Control.Feedback type="invalid">
-                    Please choose a username.
-                  </Form.Control.Feedback>
-                </InputGroup>
-              </Form.Group>
-            </Row>
-            <Row className="mb-3">
-              <Form.Group as={Col} md="6" controlId="validationCustom03">
-                <Form.Label>City</Form.Label>
-                <Form.Control type="text" placeholder="City" required />
-                <Form.Control.Feedback type="invalid">
-                  Please provide a valid city.
-                </Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group as={Col} md="3" controlId="validationCustom04">
-                <Form.Label>State</Form.Label>
-                <Form.Control type="text" placeholder="State" required />
-                <Form.Control.Feedback type="invalid">
-                  Please provide a valid state.
-                </Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group as={Col} md="3" controlId="validationCustom05">
-                <Form.Label>Zip</Form.Label>
-                <Form.Control type="text" placeholder="Zip" required />
-                <Form.Control.Feedback type="invalid">
-                  Please provide a valid zip.
-                </Form.Control.Feedback>
-              </Form.Group>
-            </Row>
-            <Form.Group className="mb-3">
-              <Form.Check
+            <Form.Group className="mb-3" controlId="validationCustom01">
+              <Form.Label>Your name</Form.Label>
+              <Form.Control
                 required
-                label="Agree to terms and conditions"
-                feedback="You must agree before submitting."
-                feedbackType="invalid"
+                type="text"
+                placeholder="Enter your name"
+                name="name"
               />
+              <Form.Control.Feedback type="invalid">
+                Please enter a name
+              </Form.Control.Feedback>
             </Form.Group>
-            <Button type="submit">Submit form</Button>
+            <Form.Group className="mb-3" controlId="formBasicLastName">
+              <Form.Label>Your last name</Form.Label>
+              <Form.Control
+                required
+                type="text"
+                placeholder="Enter your last name"
+                name="lastName"
+              />
+              <Form.Control.Feedback type="invalid">
+                Please enter your last name
+              </Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                name="email"
+                required
+                type="email"
+                placeholder="Enter email"
+              />
+              <Form.Control.Feedback type="invalid">
+                Please anter a valid email address
+              </Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                name="password"
+                required
+                type="password"
+                placeholder="Password"
+                minLength={6}
+              />
+              <Form.Control.Feedback type="invalid">
+                Please anter a valid password
+              </Form.Control.Feedback>
+              <Form.Text className="text-muted">
+                Password should have at least 6 characters
+              </Form.Text>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPasswordRepeat">
+              <Form.Label>Repeat Password</Form.Label>
+              <Form.Control
+                name="confirmPassword"
+                required
+                type="password"
+                placeholder="Repeat Password"
+                minLength={6}
+              />
+              <Form.Control.Feedback type="invalid">
+                Both passwords should match
+              </Form.Control.Feedback>
+            </Form.Group>
+
+            <Row className="pb-2">
+              <Col>
+                Do you have an account already?
+                <Link to={"/login"}> Login </Link>
+              </Col>
+            </Row>
+
+            <Button type="submit">
+              <Spinner
+                as="span"
+                animation="border"
+                size="sm"
+                role="status"
+                aria-hidden="true"
+              />
+              Submit
+            </Button>
+            <Alert show={true} variant="danger">
+                User with that email already exists!
+            </Alert>
+            <Alert show={true} variant="info">
+                User created
+            </Alert>
           </Form>
         </Col>
       </Row>
@@ -96,3 +117,4 @@ const RegisterPage = () => {
 };
 
 export default RegisterPage;
+
