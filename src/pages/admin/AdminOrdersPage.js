8d808486aa948697fccaf3d4b691fd69a698fdd8
mangala-karunarathne <mangalaicc@gmail.com>
2023-05-05 11:12:06 +0530

    CHG | Admin Orders Page

--- src/pages/admin/AdminOrdersPage.js
+++ src/pages/admin/AdminOrdersPage.js
@@ -1,11 +1,58 @@
-import React from 'react'
+import React from "react";
+import { Button, Col, Row, Table } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 
 const AdminOrdersPage = () => {
-  return (
-    <div>
-     <p> This is the Admin Orders Page</p> 
-    </div>
-  )
-}
+  const navigate = useNavigate();
 
-export default AdminOrdersPage
+  function handleClick() {
+    navigate("/user/order-details");
+  }
+
+  return (
+    <Row className="m-5">
+      <Col md={2}>Admin Links</Col>
+      <Col md={10}>
+        <h1>Orders</h1>
+        <Table striped bordered hover responsive>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>User</th>
+              <th>Date</th>
+              <th>Total</th>
+              <th>Delivered</th>
+              <th>Payment Method</th>
+              <th>Order Details</th>
+            </tr>
+          </thead>
+          <tbody>
+            {["bi bi-check-lg text-success", "bi bi-x-lg text-danger"].map(
+              (item, idx) => (
+                <tr key={idx}>
+                  <td>{idx + 1}</td>
+                  <td>Mangala Karuarathne</td>
+                  <td>2022-08-15</td>
+                  <td>$125</td>
+                  <td>
+                    <i className={item}></i>
+                  </td>
+                  <td>Paypal</td>
+                  <td>
+                    <Link to="/admin/order-details">
+                      {/* <Button variant="primary" onClick={handleClick}>Go to order</Button> */}
+                      Go to order
+                    </Link>
+                  </td>
+                </tr>
+              )
+            )}
+          </tbody>
+        </Table>
+      </Col>
+    </Row>
+  );
+};
+
+export default AdminOrdersPage;
