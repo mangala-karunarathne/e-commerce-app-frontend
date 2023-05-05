b1438ec6946833a95ae47149278c63d2f6ed5dbe
mangala-karunarathne <mangalaicc@gmail.com>
2023-05-05 13:09:27 +0530

    CHG | Admin Link Component

--- src/pages/admin/AdminOrdersPage.js
+++ src/pages/admin/AdminOrdersPage.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Button, Col, Row, Table } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 import { Link, NavLink, useNavigate } from "react-router-dom";
+import AdminLinksComponent from "../../components/Admin/AdminLinksComponent";
 
 const AdminOrdersPage = () => {
   const navigate = useNavigate();
@@ -11,47 +12,51 @@ const AdminOrdersPage = () => {
   }
 
   return (
-    <Row className="m-5">
-      <Col md={2}>Admin Links</Col>
-      <Col md={10}>
-        <h1>Orders</h1>
-        <Table striped bordered hover responsive>
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>User</th>
-              <th>Date</th>
-              <th>Total</th>
-              <th>Delivered</th>
-              <th>Payment Method</th>
-              <th>Order Details</th>
-            </tr>
-          </thead>
-          <tbody>
-            {["bi bi-check-lg text-success", "bi bi-x-lg text-danger"].map(
-              (item, idx) => (
-                <tr key={idx}>
-                  <td>{idx + 1}</td>
-                  <td>Mangala Karuarathne</td>
-                  <td>2022-08-15</td>
-                  <td>$125</td>
-                  <td>
-                    <i className={item}></i>
-                  </td>
-                  <td>Paypal</td>
-                  <td>
-                    <Link to="/admin/order-details">
-                      {/* <Button variant="primary" onClick={handleClick}>Go to order</Button> */}
-                      Go to order
-                    </Link>
-                  </td>
-                </tr>
-              )
-            )}
-          </tbody>
-        </Table>
-      </Col>
-    </Row>
+    <>
+      <Row className="m-5">
+        <Col md={2}>
+          <AdminLinksComponent />
+        </Col>
+        <Col md={10}>
+          <h1>Orders</h1>
+          <Table striped bordered hover responsive>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>User</th>
+                <th>Date</th>
+                <th>Total</th>
+                <th>Delivered</th>
+                <th>Payment Method</th>
+                <th>Order Details</th>
+              </tr>
+            </thead>
+            <tbody>
+              {["bi bi-check-lg text-success", "bi bi-x-lg text-danger"].map(
+                (item, idx) => (
+                  <tr key={idx}>
+                    <td>{idx + 1}</td>
+                    <td>Mangala Karuarathne</td>
+                    <td>2022-08-15</td>
+                    <td>$125</td>
+                    <td>
+                      <i className={item}></i>
+                    </td>
+                    <td>Paypal</td>
+                    <td>
+                      <Link to="/admin/order-details">
+                        {/* <Button variant="primary" onClick={handleClick}>Go to order</Button> */}
+                        Go to order
+                      </Link>
+                    </td>
+                  </tr>
+                )
+              )}
+            </tbody>
+          </Table>
+        </Col>
+      </Row>
+    </>
   );
 };
 
