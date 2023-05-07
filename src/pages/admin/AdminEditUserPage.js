3639311fbb8fb814ffcd8a047c57bedb02a36932
mangala-karunarathne <mangalaicc@gmail.com>
2023-05-07 11:27:28 +0530

    CHG | Admin User List Edit

--- src/pages/admin/AdminEditUserPage.js
+++ src/pages/admin/AdminEditUserPage.js
@@ -1,11 +1,80 @@
-import React from 'react'
+import React, { useState } from "react";
+import {
+  Alert,
+  Button,
+  CloseButton,
+  Col,
+  Container,
+  Form,
+  Image,
+  Row,
+  Table,
+} from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const AdminEditUserPage = () => {
-  return (
-    <div>
-      <p> This is the Admin Edit User Page</p>
-    </div>
-  )
-}
+  const [validated, setValidated] = useState(false);
 
-export default AdminEditUserPage
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+
+  return (
+    <Container>
+      <Row className="justify-content-md-center mt-5">
+        <Col md={1}>
+          <Link to="/admin/users" className="btn btn-info my-3">
+            Go Back
+          </Link>
+        </Col>
+        <Col md={6}>
+          <h1>Edit User</h1>
+          <Form noValidate validated={validated} onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="formBasicFirstName">
+              <Form.Label>First Name</Form.Label>
+              <Form.Control
+                name="firstname"
+                required
+                type="text"
+                defaultValue="Mangala"
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicLastName">
+              <Form.Label>Last Name</Form.Label>
+              <Form.Control
+                name="lastname"
+                required
+                type="text"
+                rows={3}
+                defaultValue="Karunarathne"
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                name="email"
+                required
+                type="email"
+                defaultValue="mangalaicc@gmail.com"
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" name="isAdmin" label="Is Admin" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Update
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default AdminEditUserPage;
--- src/pages/admin/AdminUserPage.js
+++ src/pages/admin/AdminUserPage.js
@@ -1,11 +1,65 @@
-import React from 'react'
+import React from "react";
+import { Button, Col, Row, Table } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import { Link, NavLink, useNavigate } from "react-router-dom";
+import AdminLinksComponent from "../../components/Admin/AdminLinksComponent";
 
-const AdminUserPage = () => {
-  return (
-    <div>
-      <p> This is the Admin User Page</p>
-    </div>
-  )
+const deleteHandler = () => {
+  if(window.confirm("Are you sure?")) alert("User Deletet !");
 }
 
-export default AdminUserPage
+const AdminUserPage = () => {
+
+  return (
+    <>
+      <Row className="m-5">
+        <Col md={2}>
+          <AdminLinksComponent />
+        </Col>
+        <Col md={10}>
+          <h1>User List</h1>
+          <Table striped bordered hover responsive>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Email</th>
+                <th>Is Admin</th>
+                <th>Edit / Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              {["bi bi-check-lg text-success", "bi bi-x-lg text-danger"].map(
+                (item, idx) => (
+                  <tr key={idx}>
+                    <td>{idx + 1}</td>
+                    <td>Mangala </td>
+                    <td>Karuarathne</td>
+                    <td>mangalaicc@gmail.com</td>
+                    <td>
+                      <i className={item}></i>
+                    </td>
+                    <td>
+                      <LinkContainer to="/admin/edit-user">
+                        <Button className="btn-sm">
+                          <i className="bi bi-pencil-square"></i>
+                        </Button>
+                      </LinkContainer>
+                      {" / "}
+                      <Button variant="danger" className="btn-sm" onClick={deleteHandler}>
+                      <i className="bi bi-x-circle"></i>
+                      </Button>
+                    </td>
+                  </tr>
+                )
+              )}
+            </tbody>
+          </Table>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default AdminUserPage;
