b73c6372bde7532a5cda357158d5d3c2cb8f334f
mangala-karunarathne <mangalaicc@gmail.com>
2023-05-07 19:40:14 +0530

    CHG | Admin Chat Page

--- src/pages/admin/AdminChatsPage.js
+++ src/pages/admin/AdminChatsPage.js
@@ -1,20 +1,22 @@
 import React from "react";
-import { Col, Row } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import AdminChatRoomComponent from "../../components/Admin/AdminChatRoomComponent";
 import AdminLinksComponent from "../../components/Admin/AdminLinksComponent";
 
 const AdminChatsPage = () => {
   return (
-    <Row>
-      <Col md={2}>
-        <AdminLinksComponent />
-      </Col>
-      <Col md={10}>
-        <Row>
-          <AdminChatRoomComponent/>
-        </Row>
-      </Col>
-    </Row>
+    <Container className="mt-5">
+      <Row>
+        <Col md={2}>
+          <AdminLinksComponent />
+        </Col>
+        <Col md={10}>
+          <Row>
+            <AdminChatRoomComponent />
+          </Row>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
