049d9e0e2cabdb64d315801b61abd8f3663e037b
mangala-karunarathne <mangalaicc@gmail.com>
2023-07-20 07:19:06 +0530

    CHG | Create Simple Reducer Function

--- src/index.js
+++ src/index.js
@@ -4,20 +4,17 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap/dist/js/bootstrap.min.js";
-
-
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <React.StrictMode>
-  <App />
-  // </React.StrictMode>
+  <Provider store={store}>
+    {/* <React.StrictMode> */}
+      <App />
+    {/* </React.StrictMode> */}
+  </Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,21 @@
 import { createStore } from "redux";
 
-const store = createStore();
+const counterReducer = (state = { value: 0 }, action) => {
+  switch (action.type) {
+    case "ADD":
+      return { value: state.value + 1 + action.someValue };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(counterReducer, { value: 0 });
+
+console.log(store.getState());
+
+store.dispatch({
+  type: "ADD",
+  someValue: 10,
+});
 
 export default store;
