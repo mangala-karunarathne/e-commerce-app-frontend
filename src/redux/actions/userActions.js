b750133de4f99bb67a58649185b1dc3f5322a475
mangala-karunarathne <mangalaicc@gmail.com>
2023-07-24 07:41:04 +0530

    CHG | Save Logged In User in Redux Global State

---
+++ src/redux/actions/userActions.js
@@ -0,0 +1,9 @@
+import { LOGIN_USER } from "../constants/userConstants"
+
+
+export const setReduxUserState = (userCreated) => (dispatch) => {
+    dispatch({
+        type: LOGIN_USER,
+        payload: userCreated
+    })
+}
