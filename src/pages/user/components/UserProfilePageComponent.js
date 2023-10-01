b21082ca8085c045140dff2608221206ba8e2c7a
mangala-karunarathne <mangalaicc@gmail.com>
2023-10-01 23:08:05 +0530

    CHG | Update Redux State and Local Storage When Updating the User

--- src/pages/user/components/UserProfilePageComponent.js
+++ src/pages/user/components/UserProfilePageComponent.js
@@ -3,8 +3,12 @@ import { useEffect, useState } from "react";
 
 const UserProfilePageComponent = ({
   updateUserApiRequest,
-  userInfo,
   fetchUser,
+  userInfoFromRedux,
+  setReduxUserState,
+  reduxDispatch,
+  localStorage,
+  sessionStorage,
 }) => {
   const [validated, setValidated] = useState({});
   const [updateUserResponseState, setUpdateUserResponseState] = useState({
@@ -13,10 +17,14 @@ const UserProfilePageComponent = ({
   });
   const [passwordMatchState, setpasswordMatchState] = useState(true);
   const [user, setUser] = useState({});
+  const userInfo = userInfoFromRedux;
 
   useEffect(() => {
     fetchUser(userInfo._id)
-      .then((data) => setUser(data))
+      .then((data) => {
+        setUser(data)
+        console.log("data", data);
+      })
       .catch((er) => console.log(er));
   }, [userInfo._id]);
 
@@ -64,6 +72,22 @@ const UserProfilePageComponent = ({
       )
         .then((data) => {
           setUpdateUserResponseState({ success: data.success, error: "" });
+          reduxDispatch(
+            setReduxUserState({
+              doNotLogout: userInfo.doNotLogout,
+              ...data.userUpdated,
+            })
+          );
+          if (userInfo.doNotLogout)
+            localStorage.setItem(
+              "userInfo",
+              JSON.stringify({ doNotLogout: true, ...data.userUpdated })
+            );
+          else
+            sessionStorage.setItem(
+              "userInfo",
+              JSON.stringify({ doNotLogout: false, ...data.userUpdated })
+            );
         })
         .catch((er) =>
           setUpdateUserResponseState({
@@ -78,155 +102,156 @@ const UserProfilePageComponent = ({
   };
   return (
     <Container>
-      <Row className="mt-5 justify-content-md-center">
-        <Col md={6}>
-          <h1>User Profile</h1>
-          <Form noValidate validated={validated} onSubmit={handleSubmit}>
-            <Form.Group className="mb-3" controlId="validationCustom01">
-              <Form.Label>Your name</Form.Label>
-              <Form.Control
-                required
-                type="text"
-                defaultValue={user.name}
-                name="name"
-              />
-              <Form.Control.Feedback type="invalid">
-                Please enter a name
-              </Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicLastName">
-              <Form.Label>Your last name</Form.Label>
-              <Form.Control
-                required
-                type="text"
-                defaultValue={user.lastName}
-                name="lastName"
-              />
-              <Form.Control.Feedback type="invalid">
-                Please enter your last name
-              </Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control
-                // name="email"
-                disabled
-                value={
-                  user.email +
-                  " : if you want to change this mail remove this mail and register again with new email"
+        <Row className="mt-5 justify-content-md-center">
+          <Col md={6}>
+            <h1>User Profile</h1>
+            <Form noValidate validated={validated} onSubmit={handleSubmit}>
+              <Form.Group className="mb-3" controlId="validationCustom01">
+                <Form.Label>Your name</Form.Label>
+                <Form.Control
+                  required
+                  type="text"
+                  defaultValue={user.name}
+                  name="name"
+                />
+                <Form.Control.Feedback type="invalid">
+                  Please enter a name
+                </Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicLastName">
+                <Form.Label>Your last name</Form.Label>
+                <Form.Control
+                  required
+                  type="text"
+                  defaultValue={user.lastName}
+                  name="lastName"
+                />
+                <Form.Control.Feedback type="invalid">
+                  Please enter your last name
+                </Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  // name="email"
+                  disabled
+                  value={
+                    user.email +
+                    " : if you want to change this mail remove this mail and register again with new email"
+                  }
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicPhone">
+                <Form.Label>Phone Number</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter Your Phone Number"
+                  defaultValue={user.phoneNumber}
+                  name="phoneNumber"
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicAddress">
+                <Form.Label>Addrress</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter Your Addrress"
+                  defaultValue={user.address}
+                  name="address"
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicCountry">
+                <Form.Label>Country</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter Your Country"
+                  defaultValue={user.country}
+                  name="country"
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicZip">
+                <Form.Label>Zip Code</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter Your Zip Code"
+                  defaultValue={user.zipCode}
+                  name="zipCode"
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicCity">
+                <Form.Label>City</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter Your City"
+                  defaultValue={user.city}
+                  name="city"
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicState">
+                <Form.Label>State</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter Your State"
+                  defaultValue={user.state}
+                  name="state"
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  name="password"
+                  required
+                  type="password"
+                  placeholder="Password"
+                  minLength={6}
+                  onChange={onChange}
+                  isInvalid={!passwordMatchState}
+                />
+                <Form.Control.Feedback type="invalid">
+                  Please enter a valid password
+                </Form.Control.Feedback>
+                <Form.Text className="text-muted">
+                  Password should have at least 6 characters
+                </Form.Text>
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicPasswordRepeat">
+                <Form.Label>Repeat Password</Form.Label>
+                <Form.Control
+                  name="confirmPassword"
+                  required
+                  type="password"
+                  placeholder="Repeat Password"
+                  minLength={6}
+                  onChange={onChange}
+                  isInvalid={!passwordMatchState}
+                />
+                <Form.Control.Feedback type="invalid">
+                  Both passwords should match
+                </Form.Control.Feedback>
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Update
+              </Button>
+              <Alert
+                show={
+                  updateUserResponseState &&
+                  updateUserResponseState.error !== ""
                 }
-              />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicPhone">
-              <Form.Label>Phone Number</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Enter Your Phone Number"
-                defaultValue={user.phoneNumber}
-                name="phoneNumber"
-              />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicAddress">
-              <Form.Label>Addrress</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Enter Your Addrress"
-                defaultValue={user.address}
-                name="address"
-              />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCountry">
-              <Form.Label>Country</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Enter Your Country"
-                defaultValue={user.country}
-                name="country"
-              />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicZip">
-              <Form.Label>Zip Code</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Enter Your Zip Code"
-                defaultValue={user.zipCode}
-                name="zipCode"
-              />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCity">
-              <Form.Label>City</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Enter Your City"
-                defaultValue={user.city}
-                name="city"
-              />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicState">
-              <Form.Label>State</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Enter Your State"
-                defaultValue={user.state}
-                name="state"
-              />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                name="password"
-                required
-                type="password"
-                placeholder="Password"
-                minLength={6}
-                onChange={onChange}
-                isInvalid={!passwordMatchState}
-              />
-              <Form.Control.Feedback type="invalid">
-                Please enter a valid password
-              </Form.Control.Feedback>
-              <Form.Text className="text-muted">
-                Password should have at least 6 characters
-              </Form.Text>
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicPasswordRepeat">
-              <Form.Label>Repeat Password</Form.Label>
-              <Form.Control
-                name="confirmPassword"
-                required
-                type="password"
-                placeholder="Repeat Password"
-                minLength={6}
-                onChange={onChange}
-                isInvalid={!passwordMatchState}
-              />
-              <Form.Control.Feedback type="invalid">
-                Both passwords should match
-              </Form.Control.Feedback>
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              Update
-            </Button>
-            <Alert
-              show={
-                updateUserResponseState && updateUserResponseState.error !== ""
-              }
-              variant="danger"
-            >
-              Something went wrong!
-            </Alert>
-            <Alert
-              show={
-                updateUserResponseState &&
-                updateUserResponseState.success === "User Updated"
-              }
-              variant="info"
-            >
-              User Profile Updated
-            </Alert>
-          </Form>
-        </Col>
-      </Row>
+                variant="danger"
+              >
+                Something went wrong!
+              </Alert>
+              <Alert
+                show={
+                  updateUserResponseState &&
+                  updateUserResponseState.success === "User Updated"
+                }
+                variant="info"
+              >
+                User Profile Updated
+              </Alert>
+            </Form>
+          </Col>
+        </Row>
     </Container>
   );
 };
