a7d4f25753639d70a20129fbc3c59ce49a9c6798
mangala-karunarathne <mangalaicc@gmail.com>
2023-10-11 07:39:45 +0530

    CHG | Seperate Product List page with its component

---
+++ src/pages/components/ProductListPageComponent.js
@@ -0,0 +1,64 @@
+import React from "react";
+import {
+  Button,
+  Col,
+  Container,
+  ListGroup,
+  Pagination,
+  Row,
+} from "react-bootstrap";
+import SortOptionsComponent from "../../components/SortOptionsComponent";
+import PriceFilterComponent from "../../components/filterQueryResultOptions/PriceFilterComponent";
+import RatingFilterComponent from "../../components/filterQueryResultOptions/RatingFilterComponent";
+import CategoryFilterComponent from "../../components/filterQueryResultOptions/CategoryFilterComponent";
+import AttributesFilterComponent from "../../components/filterQueryResultOptions/AttributesFilterComponent";
+import ProductForListComponent from "../../components/ProductForListComponent";
+import PaginationComponent from "../../components/PaginationComponent";
+
+
+const ProductListPageComponent = () => {
+
+
+  return (
+    <Container fluid>
+      <Row>
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroup.Item className="mb-3 mt-3">
+              <SortOptionsComponent />
+            </ListGroup.Item>
+            <ListGroup.Item>
+              FILTER : <br />
+              <PriceFilterComponent />
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <RatingFilterComponent />
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <CategoryFilterComponent />
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <AttributesFilterComponent />
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Button variant="primary">Filters</Button>{" "}
+              <Button variant="danger">Reset Filters</Button>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={9}>
+          {Array.from({ length: 5 }).map((_, idx) => (
+            <ProductForListComponent
+              key={idx}
+              images={["games", "monitors", "tablets", "books", "videos"]}
+              idx={idx}
+            />
+          ))}
+          <PaginationComponent />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default ProductListPageComponent;
