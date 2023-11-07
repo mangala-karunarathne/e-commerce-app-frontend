1682948bb2eb144e9857e8f0f248a620c3a7339f
mangala-karunarathne <mangalaicc@gmail.com>
2023-11-08 00:42:55 +0530

    CHG | Passing Props from Cart Page

--- src/pages/components/CartPageComponent.js
+++ src/pages/components/CartPageComponent.js
@@ -11,7 +11,7 @@ import {
 import { LinkContainer } from "react-router-bootstrap";
 import CartItemComponent from "../../components/CartItemComponent";
 
-const CartPageComponent = () => {
+const CartPageComponent = ({addToCart, cartItems, cartSubtotal, reduxDispatch}) => {
   return (
     <Container fluid>
       <Row className="mt-4">
